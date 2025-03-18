1922a9f6cc4e407290ca6f5945b2bd6f169be271
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-03-17 21:12:45 -0300

    Create ex-listas.py

---
+++ 1.3 Estruturas-Lineares/Listas/ex-listas.py
@@ -0,0 +1,17 @@
+# Criação de uma lista
+lista = [1, 2, 3, 4, 5]
+
+# Inserção de um novo item
+lista.append(6)
+
+# Remoção de um item
+lista.remove(3)
+
+# Acesso a um item
+print(lista[0])  # Saída: 1
+
+# Busca de um item
+print(lista.index(4))  # Saída: 2
+
+# Impressão da lista
+print(lista)  # Saída: [1, 2, 4, 5, 6]
