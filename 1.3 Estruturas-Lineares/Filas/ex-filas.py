744e3b955d0e132392efa4322d2f90346acf1d65
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-03-17 21:11:12 -0300

    Create ex-filas.py

---
+++ 1.3 Estruturas-Lineares/Filas/ex-filas.py
@@ -0,0 +1,16 @@
+# Criação de uma fila
+fila = []
+
+# Enqueue de itens
+fila.append(1)
+fila.append(2)
+fila.append(3)
+
+# Dequeue de um item
+print(fila.pop(0))  # Saída: 1
+
+# Peek do próximo item
+print(fila[0])  # Saída: 2
+
+# Impressão da fila
+print(fila)  # Saída: [2, 3]
