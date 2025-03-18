e5ca182d80ed2e0a722a1551ab1017844e673d1c
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-03-17 21:14:34 -0300

    Create ex.pilhas.py

---
+++ 1.3 Estruturas-Lineares/Pilhas/ex.pilhas.py
@@ -0,0 +1,16 @@
+# Criação de uma pilha
+pilha = []
+
+# Push de itens
+pilha.append(1)
+pilha.append(2)
+pilha.append(3)
+
+# Pop de um item
+print(pilha.pop())  # Saída: 3
+
+# Peek do próximo item
+print(pilha[-1])  # Saída: 2
+
+# Impressão da pilha
+print(pilha)  # Saída: [1, 2]
